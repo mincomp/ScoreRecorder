79cecd50cc1f5bcd12af38ff522cc8d7d607bac9
mincomp <mincomp@gmail.com>
2015-06-22 23:37:09 -0700

    Add GameRoundViewController

--- ScoreRecorder/ScoreRecorder/ScoreRecorder/AddPlayerTableViewController.m
+++ ScoreRecorder/ScoreRecorder/ScoreRecorder/AddPlayerTableViewController.m
@@ -129,14 +129,14 @@
 }
 */
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    UIViewController *controller = [segue destinationViewController];
+    if (segue.identifier == @"Next") {
+        
+    }
 }
-*/
 
 @end
---
+++ ScoreRecorder/ScoreRecorder/ScoreRecorder/GameRoundViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameRoundViewController.h
+//  ScoreRecorder
+//
+//  Created by Yunfeng Bai on 6/22/15.
+//  Copyright (c) 2015 Yunfeng Bai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameRoundViewController : UIViewController
+
+@end
